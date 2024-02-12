1be03ddba18fa656ad803c51f5382b002524b67a
Guillaume Scandola <gscandola@meilleursagents.com>
2024-02-12 11:04:24 +0100

    ci: is it the end ?

--- .circleci/scripts/generate_config.sh
+++ .circleci/scripts/generate_config.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit -o pipefail
 
-#$TARGET_PACKAGES must be the JSON published packages from GHAction
-target_packages=$TARGET_PACKAGES
-
 echo "Installing yq..."
 curl -s -L -o yq "https://github.com/mikefarah/yq/releases/download/v4.40.7/yq_linux_amd64"
 chmod +x ./yq
@@ -12,9 +9,12 @@ mv ./yq "${HOME}/.local/bin/"
 
 # We use yq to replace the package key, this is the only edit in place we need
 # TODO find a clever way to target appropriate job instead of using numeric index
-yq e ".workflows.build_deploy_preview.jobs[1].\"build-preview\".matrix.parameters.\"package\" = $target_packages" -i .circleci/continue_config.yml
+yq e ".workflows.build_deploy_preview.jobs[1].\"build-preview\".matrix.parameters.\"package\" = $TARGET_PACKAGES" -i .circleci/continue_config.yml
 # TODO find a clever way to target appropriate job instead of using numeric index
-yq e ".workflows.build_deploy_preview.jobs[2].\"deploy-preview\".matrix.parameters.\"package\" = $target_packages" -i .circleci/continue_config.yml
+yq e ".workflows.build_deploy_preview.jobs[2].\"deploy-preview\".matrix.parameters.\"package\" = $TARGET_PACKAGES" -i .circleci/continue_config.yml
+
+# Release Type param
+yq e ".parameters.releaseType.default = \"$RELEASE_TYPE\"" -i .circleci/continue_config.yml
 
 # Debug
 cat .circleci/continue_config.yml
