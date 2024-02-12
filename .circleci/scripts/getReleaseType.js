98fa8a2f59aa3b5d2852f6380792f57e926729d5
Guillaume Scandola <gscandola@meilleursagents.com>
2024-02-12 10:44:03 +0100

    ci: change node script output

--- .circleci/scripts/getReleaseType.js
+++ .circleci/scripts/getReleaseType.js
@@ -2,7 +2,7 @@
 Input:
 [{"name":"pkg-a","version":"0.0.2-pre.3"}]
 
-Output: "pre-release" or "release"
+Output: pre-release or release
 */
 
 const jsonString = process.argv[2];
@@ -10,4 +10,4 @@ const data = JSON.parse(jsonString);
 
 const type = data.some(item => item.version.match(/-pre\./)) ? 'pre-release' : 'release';
 
-console.log(JSON.stringify(type))
+console.log(type)
